ea32cdeb003c50438fe2548eb9748f8838fdcb17
cabbage556 <liberty556@gmail.com>
2023-11-10 16:37:04 +0900

    feat: 유저 모듈 생성
    - 유저 모듈 생성
    - 유저 서비스 생성
    - 유저 컨트롤러 생성 및 유저 서비스 의존성 주입
    
    feat-#1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './global/configs/validation.schema';
+import { UsersModule } from './users/users.module';
 import jwtConfiguration from './global/configs/jwt.configuration';
 
 @Module({
@@ -14,6 +15,7 @@ import jwtConfiguration from './global/configs/jwt.configuration';
 			validationSchema,
 		}),
 		DatabaseModule,
+		UsersModule,
 	],
 })
 export class AppModule {}
