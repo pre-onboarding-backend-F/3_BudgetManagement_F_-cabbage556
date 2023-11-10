6779ffac45795c2df6e4c2c1d78567f7d4017d53
cabbage556 <liberty556@gmail.com>
2023-11-10 16:48:43 +0900

    feat: 유저 엔티티 생성
    - 베이스 엔티티 상속
    - account, password, refreshToken 필드 추가
    - account 필드 유니크 제약조건 추가
    - 유저 서비스에 유저 레포지토리 의존성 주입
    
    feat-#1

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectRepository(User)
+		private readonly usersRepository: Repository<User>, //
+	) {}
+}
