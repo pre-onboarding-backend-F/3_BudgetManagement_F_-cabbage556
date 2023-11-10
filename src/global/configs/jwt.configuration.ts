c513999a7f7a1c6de2571c1a76bbda9eb7fcd2ed
cabbage556 <liberty556@gmail.com>
2023-11-10 21:18:14 +0900

    fix: jwtConfiguration 수정
    - 토큰 생성을 위한 시크릿 secretKey을 secret으로 수정
    - 토큰 생성을 위한 만료시간 expirationTime을 expiresIn으로 수정
    
    feat-#5

--- src/global/configs/jwt.configuration.ts
+++ src/global/configs/jwt.configuration.ts
@@ -2,12 +2,12 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwtConfiguration', () => ({
 	access: {
-		secretKey: process.env.JWT_ACCESS_SECRET_KEY,
-		expirationTime: process.env.JWT_ACCESS_EXPIRATION_TIME,
+		secret: process.env.JWT_ACCESS_SECRET_KEY,
+		expiresIn: process.env.JWT_ACCESS_EXPIRATION_TIME,
 	},
 
 	refresh: {
-		secretKey: process.env.JWT_REFRESH_SECRET_KEY,
-		expirationTime: process.env.JWT_REFRESH_EXPIRATION_TIME,
+		secret: process.env.JWT_REFRESH_SECRET_KEY,
+		expiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME,
 	},
 }));
