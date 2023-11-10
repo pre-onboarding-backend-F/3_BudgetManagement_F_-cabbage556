ecee47d3f0905ea0cde6ae45c77b2820190ada56
cabbage556 <liberty556@gmail.com>
2023-11-10 21:47:53 +0900

    fix: jwtConfiguration, validationSchema 수정
    - 환경 변수 수정에 따라 jwtConfiguration과 validationSchema 수정
    
    feat-#5

--- src/global/configs/jwt.configuration.ts
+++ src/global/configs/jwt.configuration.ts
@@ -2,12 +2,12 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwtConfiguration', () => ({
 	access: {
-		secret: process.env.JWT_ACCESS_SECRET_KEY,
-		expiresIn: process.env.JWT_ACCESS_EXPIRATION_TIME,
+		secret: process.env.JWT_ACCESS_SECRET,
+		expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
 	},
 
 	refresh: {
-		secret: process.env.JWT_REFRESH_SECRET_KEY,
-		expiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME,
+		secret: process.env.JWT_REFRESH_SECRET,
+		expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
 	},
 }));
