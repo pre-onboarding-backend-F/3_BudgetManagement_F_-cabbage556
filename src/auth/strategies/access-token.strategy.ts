ce967d2f841c2bb84d076714db2974b84f0da89e
cabbage556 <liberty556@gmail.com>
2023-11-11 13:59:45 +0900

    fix: 액세스 토큰 전략, 리프레시 토큰 전략 validate 메서드 수정
    액세스토큰 전략 validate 메서드 수정
    1. 로그아웃 시 refreshToken을 null로 설정하여 로그아웃 상태임을 저장
    2. 로그아웃 시 사용했지만 아직 만료시간이 되지 않아 여전히 유효한 액세스 토큰으로 재요청하면 DB에 저장된 refreshToken을 확인
    3. refreshToken이 null이라면 유효하지 않은 액세스 토큰으로 판단
    
    리프레시토큰 전략 validate 메서드 수정
    1. 로그아웃 시 refreshToken을 null로 설정하여 로그아웃 상태임을 저장
    2. 로그아웃했지만 아직 만료시간이 되지 않아 여전히 유효한 리프레시 토큰으로 액세스 토큰 재발급 요청 시 DB에 저장된 refreshToken을 확인
    3. refreshToken이 null이라면 유효하지 않은 리프레시 토큰으로 판단
    
    fix-#16

--- src/auth/strategies/access-token.strategy.ts
+++ src/auth/strategies/access-token.strategy.ts
@@ -5,8 +5,6 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from 'src/global';
 import jwtConfiguration from 'src/global/configs/jwt.configuration';
 import { AuthService } from '../auth.service';
-import { JsonWebTokenError } from 'jsonwebtoken';
-import { AuthException } from '../enums';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'access') {
@@ -22,11 +20,12 @@ export class AccessTokenStrategy extends PassportStrategy(Strategy, 'access') {
 	}
 
 	async validate(payload: TokenPayload) {
-		// 1. 로그아웃 시 refreshToken을 null로 설정
-		// 2. 로그아웃 시 사용한 액세스 토큰으로 재요청하면 refreshToken을 확인해 유효하지 않은 액세스 토큰으로 판단
+		// 1. 로그아웃 시 refreshToken을 null로 설정하여 로그아웃 상태임을 저장
+		// 2. 로그아웃 시 사용했지만 아직 만료시간이 되지 않아 여전히 유효한 액세스 토큰으로 재요청하면 DB에 저장된 refreshToken을 확인
+		// 3. refreshToken이 null이라면 유효하지 않은 액세스 토큰으로 판단
 		const { refreshToken } = await this.authService.checkAlreadyLogOut(payload.id);
 		if (!refreshToken) {
-			throw new JsonWebTokenError(AuthException.INVALID_ACCESS_TOKEN);
+			return false;
 		}
 
 		return payload;
