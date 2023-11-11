664d686a153e01314ed130d8875ff4e3ca13a1e3
cabbage556 <liberty556@gmail.com>
2023-11-11 19:47:11 +0900

    feat: BaseEntity의 createdAt, updatedAt, deletedAt 칼럼 직렬화
    - class-transformer의 Exclude 데코레이터를 사용해 createdAt, updatedAt, deletedAt 칼럼 제외
    - ClassSerializerInterceptor 글로벌 인터셉터 적용
    
    feat-#18

--- src/global/entities/base.entity.ts
+++ src/global/entities/base.entity.ts
@@ -1,15 +1,19 @@
 import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 export abstract class BaseEntity {
 	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
+	@Exclude()
 	@CreateDateColumn()
 	createdAt: Date;
 
+	@Exclude()
 	@UpdateDateColumn()
 	updatedAt: Date;
 
+	@Exclude()
 	@DeleteDateColumn()
 	deletedAt: Date;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter, JwtExceptionFilter, TransformInterceptor } from './global';
 
 async function bootstrap() {
@@ -9,7 +9,7 @@ async function bootstrap() {
 	const PORT = app.get(ConfigService).getOrThrow('SERVER_PORT');
 	const reflector = new Reflector();
 
-	app.useGlobalInterceptors(new TransformInterceptor(reflector));
+	app.useGlobalInterceptors(new TransformInterceptor(reflector), new ClassSerializerInterceptor(reflector));
 	app.useGlobalFilters(new HttpExceptionFilter(), new JwtExceptionFilter());
 	app.useGlobalPipes(
 		new ValidationPipe({
