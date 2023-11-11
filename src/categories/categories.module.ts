1ede80747ee386a6b963434559fd1e71b15c8cde
cabbage556 <liberty556@gmail.com>
2023-11-11 17:53:15 +0900

    feat: 카테고리 엔티티 추가
    카테고리 엔티티
    - BaseEntity 상속
    - name 칼럼
      - CategoryName enum 타입 설정
      - unique 제약조건 추가
    카테고리 서비스
    - onModuleInit 메서드
      - 카테고리 모듈 초기화 시 CategoryName enum의 값으로 DB에 upsert 쿼리 수행
    
    feat-#14

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entity';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([Category])],
 	controllers: [CategoriesController],
 	providers: [CategoriesService],
 })
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entity';
+import { Repository } from 'typeorm';
+import { CategoryName } from 'src/global';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService implements OnModuleInit {
+	constructor(
+		@InjectRepository(Category)
+		private readonly categoriesRepository: Repository<Category>, //
+	) {}
+
+	async onModuleInit(): Promise<void> {
+		const categoryNames = Object.values(CategoryName).map((name) => {
+			return { name };
+		});
+		await this.categoriesRepository.upsert(categoryNames, {
+			conflictPaths: ['name'],
+			skipUpdateIfNoValuesChanged: true,
+		});
+	}
+}
