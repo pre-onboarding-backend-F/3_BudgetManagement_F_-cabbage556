94c8c62520583289693c5d28896f3e0a087fd08f
cabbage556 <liberty556@gmail.com>
2023-11-13 02:14:25 +0900

    feat: 카테고리별 지출 엔티티
    - 카테고리별 지출 엔티티 생성
      - date 칼럼 smallint 타입 설정
      - memo 칼럼 varchar 타입 설정
      - amount 칼럼 int 타입 설정, 기본값 0 설정
      - excluding_in_total 칼럼 boolean 타입 설정
      - 월별 지출 엔티티와 ManyToOne 관계 설정
      - 카테고리 엔티티와 ManyToOne 관계 설정
    - 카테고리별 지출 서비스에 카테고리별 지출 엔티티 레포지토리 의존성 주입
    
    feat-#35

--- src/category-expenses/category-expenses.module.ts
+++ src/category-expenses/category-expenses.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoryExpensesService } from './category-expenses.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryExpense } from './entity';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([CategoryExpense])],
 	providers: [CategoryExpensesService],
 	exports: [CategoryExpensesService],
 })
--- src/category-expenses/category-expenses.service.ts
+++ src/category-expenses/category-expenses.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryExpense } from './entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryExpensesService {}
+export class CategoryExpensesService {
+	constructor(
+		@InjectRepository(CategoryExpense)
+		private readonly categoryExpensesRepository: Repository<CategoryExpense>, //
+	) {}
+}
