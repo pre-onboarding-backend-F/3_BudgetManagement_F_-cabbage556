7062c2073471a608da0ab3f3be14707e05a67d3a
cabbage556 <liberty556@gmail.com>
2023-11-12 23:32:55 +0900

    feat: 예산 설정 API
    - PUT /budgets
    - 예산 컨트롤러 액세스 토큰 가드 적용
    - setBudget 라우트 핸들러 추가
      - setBudget 서비스 로직이 반환한 월별 예산, 카테고리별 예산 응답
    - setBudget 서비스 로직 추가
      - 월별 예산이 존재하는 경우 월별 예산과 카테고리별 예산 업데이트
      - 월별 예산이 존재하지 않는 경우 월별 예산과 카테고리별 예산 생성 후 저장
    
    feat-#26

--- src/budgets/budgets.module.ts
+++ src/budgets/budgets.module.ts
@@ -3,9 +3,10 @@ import { BudgetsController } from './budgets.controller';
 import { BudgetsService } from './budgets.service';
 import { MonthlyBudgetsModule } from 'src/monthly-budgets';
 import { CategoryBudgetsModule } from 'src/category-budgets';
+import { CategoriesModule } from 'src/categories';
 
 @Module({
-	imports: [MonthlyBudgetsModule, CategoryBudgetsModule],
+	imports: [MonthlyBudgetsModule, CategoryBudgetsModule, CategoriesModule],
 	controllers: [BudgetsController],
 	providers: [BudgetsService],
 })
