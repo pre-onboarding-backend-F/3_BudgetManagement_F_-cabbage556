32cafb49cfeab9cc2cd22448ce68dc4f82026c25
cabbage556 <liberty556@gmail.com>
2023-11-14 00:32:24 +0900

    feat: 지출 기록 수정 API
    - PATCH /expenses/:id
    - updateExpense 라우트 핸들러 추가
      - updateExpense 서비스 로직이 반환한 수정한 월별 지출, 카테고리별 지출 응답
    - updateExpense 서비스 로직 추가
      - id에 해당하는 카테고리별 지출을 찾지 못하면 NotFound 예외를 던짐
      - 요청한 유저의 지출이 아니라면 Unauthorized 예외를 던짐
      - 수정한 날짜로 전달한 year, month가 월별 지출의 year, month와 일치하지 않는 경우 업데이트할 수 없으므로 BadRequest 예외를 던짐
        - year, month가 월별 지출의 year, month와 일치하지 않는 경우라면 지출 기록을 새롭게 생성해야 함
      - 수정할 필요가 없는 경우 수정하지 않고 '수정할 값이 없습니다' 문자열 응답
      - 이후 카테고리별 지출, 월별 지출 수정 후 응답
    
    feat-#40

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
-import { CreateExpenseDto } from './dto';
+import { CreateExpenseDto, UpdateExpenseDto } from './dto';
 import { GetUser, ResponseMessage } from 'src/global';
 import { User } from 'src/users';
 import { ExpenseResponse } from './enums';
 import { AccessTokenGuard } from 'src/auth';
+import { InvalidParseUUIDPipe } from './pipes';
 
 @Controller('expenses')
 @UseGuards(AccessTokenGuard)
@@ -20,5 +21,15 @@ export class ExpensesController {
 		@GetUser() user: User,
 	) {
 		return await this.expensesService.createExpense(createExpenseDto, user);
+	}
+
+	@Patch(':id')
+	@ResponseMessage(ExpenseResponse.UPDATE_EXPENSE)
+	async updateExpense(
+		@Param('id', InvalidParseUUIDPipe) id: string,
+		@Body() updateExpenseDto: UpdateExpenseDto,
+		@GetUser() user: User,
+	) {
+		return await this.expensesService.updateExpense(id, updateExpenseDto, user);
 	}
 }
--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CategoryExpense, CategoryExpensesService } from 'src/category-expenses';
 import { MonthlyExpense, MonthlyExpensesService } from 'src/monthly-expenses';
-import { CreateExpenseDto } from './dto';
+import { CreateExpenseDto, UpdateExpenseDto } from './dto';
 import { User } from 'src/users';
 import { YearMonthDay, getDate } from 'src/global';
 import { CategoriesService } from 'src/categories';
+import { ExpenseException } from './enums';
 
 @Injectable()
 export class ExpensesService {
@@ -52,16 +53,14 @@ export class ExpensesService {
 
 	// 월별 지출 전체 금액 업데이트
 	async updateMonthlyExpenseTotalAmount(
-		oldMonthlyExpense: MonthlyExpense,
+		monthlyExpense: MonthlyExpense,
 		partialDto: Partial<Omit<CreateExpenseDto, 'yyyyMMDD'>>,
 	) {
 		const { amount } = partialDto;
 
 		// 월별 지출 전체 금액 업데이트
-		oldMonthlyExpense.totalAmount += amount;
-		const monthlyExpense = await this.monthlyExpensesService.saveOne(oldMonthlyExpense);
-
-		return monthlyExpense;
+		monthlyExpense.totalAmount += amount;
+		return await this.monthlyExpensesService.saveOne(monthlyExpense);
 	}
 
 	// 새로운 월별 지출 생성 후 저장
@@ -104,5 +103,97 @@ export class ExpensesService {
 		const categoryExpense = await this.categoryExpensesService.saveOne(createdCategoryExpense);
 
 		return categoryExpense;
+	}
+
+	async updateExpense(id: string, updateExpenseDto: UpdateExpenseDto, user: User) {
+		// id에 해당하는 카테고리별 지출을 찾을 수 없으면 NotFound 예외를 던짐
+		const categoryExpenseExists = await this.categoryExpensesService.exists({ id });
+		if (!categoryExpenseExists) {
+			throw new NotFoundException(ExpenseException.NOT_FOUND);
+		}
+
+		const categoryExpense = await this.categoryExpensesService.findOne(
+			{ id },
+			{
+				// 월별 지출, 유저 join
+				monthlyExpense: {
+					user: true,
+				},
+				// 카테고리 join
+				category: true,
+			},
+		);
+
+		// 요청한 유저의 월별 지출이 아니라면 수정할 수 없으므로 Unauthorized 예외를 던짐
+		const userIdMatched = categoryExpense.monthlyExpense.user.id === user.id;
+		if (!userIdMatched) {
+			throw new UnauthorizedException(ExpenseException.CANNOT_UPDATE_OTHERS);
+		}
+
+		const { yyyyMMDD, category: categoryName, amount, memo, exclude: excludingInTotal } = updateExpenseDto;
+		const { year, month, day: date } = getDate(yyyyMMDD);
+
+		// 수정할 날짜로 전달한 year, month가 월별 지출의 year, month와 일치하지 않는 경우 업데이트할 수 없으므로 BadRequest 예외를 던짐
+		// year, month가 월별 지출의 year, month와 일치하지 않는 경우라면 지출 기록을 새롭게 생성해야 함
+		const yearMonthNotMatched = this.checkYearMonthNotMatched(categoryExpense.monthlyExpense, { year, month });
+		if (yearMonthNotMatched) {
+			throw new BadRequestException(ExpenseException.INVALID_YEAR_MONTH);
+		}
+
+		// 수정할 필요가 없다면 수정하지 않음
+		const valuesMatched = this.checkValuesMatched(categoryExpense, {
+			day: date,
+			category: categoryName,
+			amount,
+			memo,
+			exclude: excludingInTotal,
+		});
+		if (valuesMatched) {
+			return '수정할 값이 없습니다.';
+		}
+
+		// 카테고리별 지출 업데이트
+		// 이전 카테고리와 다른 경우 카테고리 업데이트
+		let category = categoryExpense.category;
+		if (categoryExpense.category.name !== categoryName) {
+			category = await this.categoriesService.findOne({ name: categoryName });
+		}
+		await this.categoryExpensesService.updateOne(categoryExpense.id, {
+			date,
+			amount,
+			memo,
+			excludingInTotal,
+			category,
+		});
+
+		// 월별 지출 전체 금액 업데이트
+		const amountDifference = amount - categoryExpense.amount;
+
+		return {
+			monthlyExpense: await this.updateMonthlyExpenseTotalAmount(categoryExpense.monthlyExpense, {
+				amount: amountDifference,
+			}),
+			categoryExpense: await this.categoryExpensesService.findOne({ id: categoryExpense.id }, { category: true }),
+		};
+	}
+
+	checkYearMonthNotMatched(monthlyExpense: MonthlyExpense, yearMonth: Omit<YearMonthDay, 'day'>): boolean {
+		const yearNotMatched = monthlyExpense.year !== yearMonth.year;
+		const monthNotMatched = monthlyExpense.month !== yearMonth.month;
+		return yearNotMatched || monthNotMatched;
+	}
+
+	checkValuesMatched(
+		categoryExpense: CategoryExpense,
+		values: Pick<YearMonthDay, 'day'> & Omit<UpdateExpenseDto, 'yyyyMMDD'>,
+	): boolean {
+		const { day: date, category: categoryName, amount, memo, exclude: excludingInTotal } = values;
+		const dateMatched = categoryExpense.date === date;
+		const categoryNameMatched = categoryExpense.category.name === categoryName;
+		const amountMatched = categoryExpense.amount === amount;
+		const memoMatched = categoryExpense.memo === memo;
+		const excludingInTotalMatched = categoryExpense.excludingInTotal === excludingInTotal;
+
+		return dateMatched && categoryNameMatched && amountMatched && memoMatched && excludingInTotalMatched;
 	}
 }
