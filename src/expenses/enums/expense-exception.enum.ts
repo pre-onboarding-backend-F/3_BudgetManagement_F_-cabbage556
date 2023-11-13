4652b24438e7a012355d80b72cf7943e0d50e567
cabbage556 <liberty556@gmail.com>
2023-11-14 01:20:57 +0900

    feat: 지출 기록 삭제 API
    - DELETE /expenses/:id
    - deleteExpense 라우트 핸들러 추가
      - deleteExpense 서비스 로직을 호출
    - deleteExpense 서비스 로직
      - id에 해당하는 카테고리별 지출을 찾지 못하면 NotFound 예외를 던짐
      - 요청한 유저의 지출이 아니라면 Unauthorized 예외를 던짐
      - id에 해당하는 카테고리별 지출 soft 삭제
      - 삭제한 카테고리별 지출 금액만큼 월별 지출 금액 차감
    
    feat-#42

--- src/expenses/enums/expense-exception.enum.ts
+++ src/expenses/enums/expense-exception.enum.ts
@@ -3,4 +3,5 @@ export enum ExpenseException {
 	NOT_FOUND = '존재하지 않는 지출입니다.',
 	CANNOT_UPDATE_OTHERS = '자신의 지출 기록만 수정할 수 있습니다.',
 	INVALID_YEAR_MONTH = '유효하지 않은 년도 또는 월입니다.',
+	CANNOT_DELETE_OTHERS = '자신의 지출 기록만 삭제할 수 있습니다.',
 }
