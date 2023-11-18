c45a90b9f2ad620ee267d065fd867e175510fa02
cabbage556 <liberty556@gmail.com>
2023-11-19 00:40:59 +0900

    feat: 월별 지출, 카테고리별 지출 더미데이터 생성
    - 랜덤 월별 지출 10개 생성 및 저장
    - 랜덤 카테고리별 지출 생성 및 저장(각 카테고리마다 3일 간격으로 생성)
      - 카테고리별 지출을 통해 월별 지출 전체 금액 계산 후 업데이트
    
    feat-#61

--- dataSource.ts
+++ dataSource.ts
@@ -2,11 +2,20 @@ import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 import { User } from 'src/users';
-import { MonthlyBudgetsFactory, MainSeeder, UsersFactory, CategoryBudgetsFactory } from 'src/database';
+import {
+	MonthlyBudgetsFactory,
+	MainSeeder,
+	UsersFactory,
+	CategoryBudgetsFactory,
+	CategoryExpensesFactory,
+	MonthlyExpensesFactory,
+} from 'src/database';
 import { MonthlyBudget } from 'src/monthly-budgets';
 import { CategoryBudget } from 'src/category-budgets';
 import { Category } from 'src/categories';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { MonthlyExpense } from 'src/monthly-expenses';
+import { CategoryExpense } from 'src/category-expenses';
 
 config({
 	path: '.development.env',
@@ -19,9 +28,15 @@ const options: DataSourceOptions & SeederOptions = {
 	username: process.env.POSTGRESQL_USER,
 	password: process.env.POSTGRESQL_PASSWORD,
 	database: process.env.POSTGRESQL_DATABASE,
-	entities: [User, MonthlyBudget, CategoryBudget, Category],
+	entities: [User, MonthlyBudget, CategoryBudget, Category, MonthlyExpense, CategoryExpense],
 	namingStrategy: new SnakeNamingStrategy(),
-	factories: [UsersFactory, MonthlyBudgetsFactory, CategoryBudgetsFactory],
+	factories: [
+		UsersFactory,
+		MonthlyBudgetsFactory,
+		CategoryBudgetsFactory,
+		MonthlyExpensesFactory,
+		CategoryExpensesFactory,
+	],
 	seeds: [MainSeeder],
 };
 export const dataSource = new DataSource(options);
--- src/database/seeders/main.seeder.ts
+++ src/database/seeders/main.seeder.ts
@@ -4,16 +4,24 @@ import { User } from 'src/users';
 import { MonthlyBudget } from 'src/monthly-budgets';
 import { CategoryBudget } from 'src/category-budgets';
 import { Category } from 'src/categories';
+import { MonthlyExpense } from 'src/monthly-expenses';
+import { CategoryExpense } from 'src/category-expenses';
+import { getToday } from 'src/global';
+import { faker } from '@faker-js/faker';
 
 export class MainSeeder implements Seeder {
 	async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
 		const categoriesRepository = dataSource.getRepository(Category);
 		const monthlyBudgetsRepository = dataSource.getRepository(MonthlyBudget);
 		const categoryBudgetsRepository = dataSource.getRepository(CategoryBudget);
+		const monthlyExpensesRepository = dataSource.getRepository(MonthlyExpense);
+		const categoryExpensesRepository = dataSource.getRepository(CategoryExpense);
 
 		const usersFactory = factoryManager.get(User);
 		const monthlyBudgetsFactory = factoryManager.get(MonthlyBudget);
 		const categoryBudgetsFactory = factoryManager.get(CategoryBudget);
+		const monthlyExpensesFactory = factoryManager.get(MonthlyExpense);
+		const categoryExpensesFactory = factoryManager.get(CategoryExpense);
 
 		// 랜덤 유저 10명 저장
 		const users = await usersFactory.saveMany(10);
@@ -51,12 +59,64 @@ export class MainSeeder implements Seeder {
 				// 랜덤 카테고리별 예산 저장
 				await categoryBudgetsRepository.save(fakeCategoryBudgets);
 
-				// 월별 예산 전첵 금액 업데이트
+				// 월별 예산 전체 금액 업데이트
 				monthlyBudget.totalAmount = totalAmount;
 			}),
 		);
 
 		// 월별 예산 저장
 		await monthlyBudgetsRepository.save(monthlyBudgets);
+
+		// 랜덤 월별 지출 10개 생성
+		const { month, day } = getToday();
+		const fakeMonthlyExpenses = await Promise.all(
+			Array(10)
+				.fill('')
+				.map(async (_, i) => {
+					const fakeUser = users[i];
+					return await monthlyExpensesFactory.make({
+						user: fakeUser,
+						totalAmount: 0,
+						month,
+					});
+				}),
+		);
+		// 랜덤 월별 지출 10개 저장
+		const monthlyExpenses = await monthlyExpensesRepository.save(fakeMonthlyExpenses);
+
+		await Promise.all(
+			monthlyExpenses.map(async (monthlyExpense) => {
+				// 랜덤 카테고리별 지출 생성
+				const fakeCategoryExpenses: CategoryExpense[] = [];
+				for (let i = 0; i < day; i += 3) {
+					fakeCategoryExpenses.push(
+						...(await Promise.all(
+							categories.map(async (category) => {
+								return await categoryExpensesFactory.make({
+									category,
+									monthlyExpense,
+									date: faker.number.int({ min: 1, max: day }),
+								});
+							}),
+						)),
+					);
+				}
+
+				// 카테고리별 지출 전체 금액 계산
+				const totalAmount = fakeCategoryExpenses.reduce(
+					(prev, curr) => (prev += curr.excludingInTotal ? 0 : curr.amount),
+					0,
+				);
+
+				// 랜덤 카테고리별 지출 저장
+				await categoryExpensesRepository.save(fakeCategoryExpenses);
+
+				// 월별 지출 전체 금액 업데이트
+				monthlyExpense.totalAmount = totalAmount;
+			}),
+		);
+
+		// 월별 지출 저장
+		await monthlyExpensesRepository.save(monthlyExpenses);
 	}
 }
