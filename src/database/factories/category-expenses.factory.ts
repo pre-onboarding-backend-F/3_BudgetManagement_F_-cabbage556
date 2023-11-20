bfc35ff4204a2419b06a984b4e779807335fa4ba
cabbage556 <liberty556@gmail.com>
2023-11-20 14:43:40 +0900

    fix: DB 시드 카테고리별 지출 팩토리 함수
    - 카테고리별 지출 금액이 너무 높게 생성되어 amount 칼럼 값 조정
      - 1000원부터 20000원까지 랜덤 선택
    
    feat-#61

--- src/database/factories/category-expenses.factory.ts
+++ src/database/factories/category-expenses.factory.ts
@@ -9,10 +9,10 @@ export const CategoryExpensesFactory = setSeederFactory(CategoryExpense, (faker)
 	categoryExpense.date = faker.number.int({ min: 1, max: day }); // 1일부터 오늘 날짜까지
 	categoryExpense.memo = faker.word.noun(); // 랜덤 단어
 
-	const amounts: number[] = Array(50)
+	const amounts: number[] = Array(20)
 		.fill(1000)
 		.map((amount, i) => amount * (i + 1));
-	categoryExpense.amount = faker.helpers.arrayElement(amounts); // 1000원부터 5만원까지 1000원 차이의 금액 중 랜덤 선택
+	categoryExpense.amount = faker.helpers.arrayElement(amounts); // 1000원부터 20000원까지 1000원 차이의 금액 중 랜덤 선택
 	categoryExpense.excludingInTotal = faker.datatype.boolean(0.1); // true 선택 10%
 
 	return categoryExpense;
